8708edfa8c5311482688e43cd702383950d078ea
Mitsuaki Tanaka <130664643+mitsuTanak@users.noreply.github.com>
2024-08-08 11:04:36 -0300

    oj

---
+++ python_01/16_carros.py
@@ -0,0 +1,10 @@
+# Lista
+carros = ["BMW X6", "Audi A3", "Mini Cooper", "Land Rouver"]
+
+# Carros relacionados
+carro1 = "BMW X6"
+carro2 = "Mercedes C180"
+
+# Exive na tela quais carros estão na lista
+print(carro1 in carros)
+print(carro2 in carros)
